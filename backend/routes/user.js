943b803cb723c9b70f599e569c779db3be8821cc
Yadneya Joshi <61934942+Yadnu@users.noreply.github.com>
2024-03-05 15:27:16 +0530

    Added Signin route

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -39,6 +39,35 @@ router.post("/signup", async (req, res)=>{
         token: token
     })
 })
+const signinBody = zod.object({
+    username: zod.string().email(),
+    password: zod.string()
+})
 
+router.post("/signin", async (req, res) =>{
+    const { success } = signinBody.safeParse(req.body);
+    if(!success){
+        return res.status(411).json({
+            message: "Email already taken / Incorrect inputs"
+        })
+    }
+
+    const user = await User.findOne({
+        username: req.body.username,
+        password: req.body.password
+    })
+    if(user){
+        const token = jwt.sign({
+            userId: user._id
+        }, JWT_SECRET);
+        res.json({
+            token:token
+        })
+        return;
+    }
+    res.status(411).json({
+        message: "Error while logging in"
+    })
+})
 
 module.exports = router;
